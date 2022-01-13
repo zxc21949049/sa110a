13c68670ed799ec0826f8bcfa00c6577fd0aace2
zxc <zxd21949049@gmail.com>
2022-01-13 22:36:00 +0800

    edit

--- fin/name_function_testing.py
+++ fin/name_function_testing.py
@@ -3,7 +3,7 @@ from name_function import get_formatted_name
 
 class NamesTestCase(unittest.TestCase):
     def test_family_given_name(self): 
-        formatted_name = get_formatted_name('nao','zedong') # 毛澤東在英文中寫作Mao Zedong
+        formatted_name = get_formatted_name('mao','zedong') # 毛澤東在英文中寫作Mao Zedong
         self.assertEqual(formatted_name, 'Mao Zedong')  # self.assertEqual內部特有檢驗代碼
 
 if __name__ == '__main__':
